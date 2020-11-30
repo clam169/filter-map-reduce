8519852a2a9ab1c34bde7219d5f2fd8eab169bb5
clam169 <71351008+clam169@users.noreply.github.com>
2020-11-29 16:32:03 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,57 @@
+// Observe the list of people below
+const people = [ { name: "John Doe", age: 16 },
+                 { name: "Thomas Calls", age: 19 },
+                 { name: "Liam Smith", age: 20 },
+                 { name: "Jessy Pinkman", age: 18 }
+                ];
+
+// Observe the coffeeLovers list containing people who are coffee lovers
+const coffeeLovers = ["John Doe", "Liam Smith", "Jessy Pinkman"];
+
+// use map, filter, and reduce to do the following:
+/*
+1. Filter all people above the age of 18 (use a function called ageAbove18)
+2. Transform the people array so all people in the array who are in coffeeLovers have a property
+   in their object called coffeeLover, set either to true or false. (Use a function called addCoffeeLoverProperty)
+3. Get the total summed age of all people who are coffee lovers and above the age of 18 (Use a function called ageReducer)
+*/
+
+const ageAbove18 = ((itemInList) => {
+    return itemInList.age > 18;
+})
+
+let peopleAbove18 = people.filter(ageAbove18);
+console.log(`peopleabove18:`);
+console.log(peopleAbove18);
+
+const addCoffeeLoverProperty = ((person) => {
+    person.coffeeLover = coffeeLovers.includes(person.name);
+    return person;
+});
+
+let coffeeLoverProperty = people.map(addCoffeeLoverProperty);
+console.log(`CoffeeLoverProperty:`);
+console.log(coffeeLoverProperty);
+
+const coffeeLover = (person) => {
+    if (person.coffeeLover === true) {
+        return person.name;
+    }
+}
+
+const coffeeLoversAbove18 = people
+    .filter(ageAbove18)
+    .map(addCoffeeLoverProperty)
+    .filter(coffeeLover);
+
+const ageReducer = ((total, age) => {
+    return total + age.age;
+});
+
+const totalAgeOfCoffeeLoversAbove18 = coffeeLoversAbove18.reduce(ageReducer, 0);
+
+console.log("coffeeLoversAbove18");
+console.log(coffeeLoversAbove18);
+console.log("total sum of age");
+console.log(totalAgeOfCoffeeLoversAbove18);
+
